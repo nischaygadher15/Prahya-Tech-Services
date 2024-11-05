239693fcd170c9aa659815ff52113abafca62996
nischay gadher <nischaygadher15@gmail.com>
2024-11-05 09:42:29 +0530

    cors issue for vercel fixed.

--- Backend/app.js
+++ Backend/app.js
@@ -30,7 +30,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://pts-frontend.vercel.app/"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/auth", authRouter);
